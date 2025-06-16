fc0e9a039c2060a9aa217281f49041b273111729
chrissstrahl <Chrissstrahl@users.noreply.github.com>
2025-06-16 12:41:37 +0200

    Fixed typo, added too few parameters error for scripts

--- coop/code/coop_generalstrings.hpp
+++ coop/code/coop_generalstrings.hpp
@@ -7,7 +7,7 @@
 
 constexpr auto _COOP_COOP_allowesOnlyBlue = "Coop Mod allowes only blue team.\n";
 
-constexpr auto _COOP_INFO_TEST_maptype = "TEST HZM Coop Gen7 - Current map type is[% s]\n";
+constexpr auto _COOP_INFO_TEST_maptype = "TEST HZM Coop Gen7 - Current map type is[%s]\n";
 constexpr auto _COOP_INFO_TEST_maptypedetect = "TEST HZM Coop Gen7 - Detected category %s\n";
 
 constexpr auto _COOP_INFO_INIT_game = "==== HaZardModding Coop Mod Gen7 GAME - initialising ====\n";
@@ -15,6 +15,8 @@ constexpr auto _COOP_INFO_INIT_gamedone = "==== HaZardModding Coop Mod Gen7 GAME
 constexpr auto _COOP_INFO_INIT_world = "==== HaZardModding Coop Mod Gen7 WORLD - %s ====\n";
 constexpr auto _COOP_INFO_INIT_status = "==== HaZardModding Coop Mod Gen7 STAUS - %s ====\n";
 
+
+constexpr auto _COOP_WARNING_SCRIPT_ERROR_PARAMETER_TOO_FEW = "%s - too few parameters\n";
 
 constexpr auto _COOP_WARNING_FILE_failed = "HZM Coop Gen7 - WARNING: Failed to load %s\n";
 
