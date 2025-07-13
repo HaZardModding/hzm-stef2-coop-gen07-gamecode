a4f386c339a8ddf9118958283492e38de270195a
chrissstrahl <Chrissstrahl@users.noreply.github.com>
2025-07-14 00:30:55 +0200

    Added: Info text

--- coop/code/coop_generalstrings.hpp
+++ coop/code/coop_generalstrings.hpp
@@ -16,6 +16,9 @@ constexpr auto _COOP_INFO_INIT_world = "==== HaZardModding Coop Mod Gen7 WORLD -
 constexpr auto _COOP_INFO_INIT_status = "==== HaZardModding Coop Mod Gen7 STAUS - %s ====\n";
 
 
+constexpr auto _COOP_INFO_needAdmin = "^3You need to !login as Coop Admin to use this command.\n";
+
+
 constexpr auto _COOP_WARNING_SCRIPT_ERROR_PARAMETER_TOO_FEW = "%s - too few parameters\n";
 
 constexpr auto _COOP_WARNING_FILE_failed = "HZM Coop Gen7 - WARNING: Failed to load %s\n";
