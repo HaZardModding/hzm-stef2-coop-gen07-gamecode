afdd1d5115b636a1296d5cf529d28a7e85aba4fa
chrissstrahl <Chrissstrahl@users.noreply.github.com>
2025-07-17 02:38:47 +0200

    Removed deprecated code line

--- dlls/game/player.cpp
+++ dlls/game/player.cpp
@@ -11705,7 +11705,6 @@ void Player::handleDialogSetup( Entity* entity, const str& soundName )
 	//--------------------------------------------------------------
 	float fLength = gamefix_dialogGetSoundlength((char*)soundName.c_str());
 	if (fLength >= 0)
-	if ( gi.SoundLength( soundName.c_str() ) >= 0 )
 	{
 		_dialogEntnum = entity->entnum;
 		_dialogSoundIndex = gi.soundindex( soundName.c_str() );
