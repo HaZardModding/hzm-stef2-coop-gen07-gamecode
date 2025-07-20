149c89d7df442f5cdce17f05e784e7cd48b49ca5
chrissstrahl <Chrissstrahl@users.noreply.github.com>
2025-07-20 17:18:47 +0200

    Added: New coop Specific strings

--- coop/code/coop_generalstrings.hpp
+++ coop/code/coop_generalstrings.hpp
@@ -21,8 +21,30 @@ constexpr auto _COOP_INFO_adminLoggedInHost = "^3You are now logged in (Host aut
 constexpr auto _COOP_INFO_adminNeeded = "^3You need to !login as Coop Admin to use this command.\n";
 constexpr auto _COOP_INFO_coopCommandOnly = "^3This command is only usable during Coop.\n";
 
+constexpr auto _COOP_INFO_usedCommand_noclip = "^8Command: ^5noclip ^8toggled.\n";
+constexpr auto _COOP_INFO_usedCommand_stuck1 = "^5Coop^2: You have been moved to your spawn location.\n";
+constexpr auto _COOP_INFO_usedCommand_stuck1_deu = "^5Coop^2: Sie wurden zu Ihrem Spawnpunkt geschoben.\n";
+constexpr auto _COOP_INFO_usedCommand_stuck2 = "^5Coop^2: You have been moved to spawn location %d.\n";
+constexpr auto _COOP_INFO_usedCommand_stuck2_deu = "^5Coop^2: Sie wurden zu Spawnpunkt %d geschoben.\n";
+constexpr auto _COOP_INFO_usedCommand_transport1 = "^5Coop:^8 Your mobile ^3Heisenberg Compensator is recalibrating^8, please wait!\n";
+constexpr auto _COOP_INFO_usedCommand_transport1_deu = "^5Coop:^8 Ihr mobiler ^3Heisenberg Kompensator rekalibriert^8 gerade, bitte warten!\n";
+constexpr auto _COOP_INFO_usedCommand_transport2 = "^5Coop^2: No Teammember found, to serve as Transport target.\n";
+constexpr auto _COOP_INFO_usedCommand_transport2_deu = "^5Coop^2: Kein Teammitglied als Transport Ziel gefunden.\n";
+constexpr auto _COOP_INFO_usedCommand_transport3 = "^5Coop^2: Player does not allow, direct Transports.\n";
+constexpr auto _COOP_INFO_usedCommand_transport3_deu = "^5Coop^2: Spieler erlaubt keine direkt Transporte.\n";
+constexpr auto _COOP_INFO_usedCommand_transport4 = "^5Coop^2: Your Transport inhibitor is active!!!\n";
+constexpr auto _COOP_INFO_usedCommand_transport4_deu = "^5Coop^2: Ihr Transport Inhibitor ist aktiviert!!!\n";
+constexpr auto _COOP_INFO_usedCommand_transport5 = "^5Coop^2: Can't transport to your self.\n";
+constexpr auto _COOP_INFO_usedCommand_transport5_deu = "^5Coop^2: Transport zu sich selbst nicht erlaubt.\n";
+constexpr auto _COOP_INFO_usedCommand_notransport1 = "^5Coop^2: Transport inhibitor activated.\n";
+constexpr auto _COOP_INFO_usedCommand_notransport1_deu = "^5Coop^2: Transport Inhibitor aktiviert.\n";
+constexpr auto _COOP_INFO_usedCommand_notransport2 = "^5Coop^2: Transport inhibitor turned off.\n";
+constexpr auto _COOP_INFO_usedCommand_notransport2_deu = "^5Coop^2: Transport Inhibitor ausgeschaltet.\n";
+
+constexpr auto _COOP_INFO_spawnspotNumberedNotFound = "Spawnspot (in script/bsp) with number %d was not found...\n";
+
 constexpr auto _COOP_INFO_radarFollowMarker_off = "^2Green ^5locaction ^5marker ^5on Radar ^1disabled\n";
-constexpr auto _COOP_INFO_radarFollowMarker_off_deu = "^2Gruene ^5Positions ^5Markerkierung ^5auf Radar ^1deaktiviert";
+constexpr auto _COOP_INFO_radarFollowMarker_off_deu = "^2Gruene ^5Positions ^5Markerkierung ^5auf Radar ^1deaktiviert\n";
 constexpr auto _COOP_INFO_radarFollowMarker_on = "^5Locaction marked ^2green ^5on Radar of^8";
 constexpr auto _COOP_INFO_radarFollowMarker_on_deu = "^5Position ^2gruen ^5markiert auf Radar von^8";
 
@@ -34,6 +56,7 @@ constexpr auto _COOP_INFO_adminLogin_authFailure = "Login failed - Accsess denie
 constexpr auto _COOP_INFO_adminLogin_useLogout = "^3You are already logged in - use ^2!logout ^3 to log out.\n";
 constexpr auto _COOP_INFO_adminLogin_loginStartedEnter = "Login Started - Please enter the code.";
 constexpr auto _COOP_INFO_adminLogin_needLatestCoop = "You need the latest HZM Coop Mod to use !login.\n";
+constexpr auto _COOP_INFO_adminLogin_needAdminUse = "You need login as Admin to use '%s'!\n";
 constexpr auto _COOP_INFO_adminLogin_logoutDone = "^3You are now logged out.\n";
 constexpr auto _COOP_INFO_adminLogin_logoutAlready = "^3You are already logged out.\n";
 
