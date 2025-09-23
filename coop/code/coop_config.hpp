f107121f36dd780c64780275097d2f1a236f1bdb
chrissstrahl <Chrissstrahl@users.noreply.github.com>
2025-09-23 17:47:56 +0200

    Updated versions Number

--- coop/code/coop_config.hpp
+++ coop/code/coop_config.hpp
@@ -4,8 +4,8 @@
 #include "../../dlls/game/_pch_cpp.h"
 #include "coop_generalstrings.hpp"
 
-constexpr auto _COOP_THIS_VERSION = 70000;
-constexpr auto _COOP_CLIENT_MINIMUM_COMPATIBELE_VERSION = 70000;
+constexpr auto _COOP_THIS_VERSION = 70001;
+constexpr auto _COOP_CLIENT_MINIMUM_COMPATIBELE_VERSION = 70001;
 
 constexpr auto _COOP_SCRIPT_main = "globalCoop_main";
 
